af9418a8f9eb8840f9dcfb5c3ebabf7728f49ad0
Logmania1 <158375314+Logmania1@users.noreply.github.com>
2024-09-18 21:01:16 -0400

    edited app.js

--- dinero/src/App.js
+++ dinero/src/App.js
@@ -3,22 +3,50 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<h2>Expense Tracker</h2>
+
+<div class="container">
+  <h4>Your Balance</h4>
+  <h1 id="balance">$0.00</h1>
+
+  <div class="inc-exp-container">
+    <div>
+      <h4 class="moneyhead">Income</h4>
+      <p id="money-plus" class="money plus">+$0.00</p>
     </div>
+    <div>
+      <h4 class="moneyhead">Expense</h4>
+      <p id="money-minus" class="money minus">-$0.00</p>
+    </div>
+  </div>
+
+  <h3>History</h3>
+  <ul id="list" class="list">
+    {/* <!-- <li class="minus">
+      Cash <span>-$400</span><button class="delete-btn">x</button>
+    </li> --> */}
+  </ul>
+
+  <h3>Add new transaction</h3>
+  <form id="form">
+    <div class="form-control">
+      <label for="text">Category</label>
+      <input type="text" id="text" placeholder="Enter text..." />
+    </div>
+    <div class="form-control">
+      <label for="amount"
+        >Amount <br />
+        (negative - expense, positive - income)</label
+      >
+      <input type="number" step="0.01" id="amount" placeholder="Enter amount..." />
+    </div>
+    <button class="btn">Add transaction</button>
+  </form>
+</div>
+
+{/* <script src="index.js"></script>  */}
+</div>
   );
 }
 
